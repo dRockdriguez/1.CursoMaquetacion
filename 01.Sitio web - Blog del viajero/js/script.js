d2438926469cb8fe46d58d959c74094346ebd79b
drockdriguez <david.rodriguez@grupoonetec.com>
2019-04-23 10:26:07 +0200

    efecto banner

--- 01.Sitio web - Blog del viajero/js/script.js
+++ 01.Sitio web - Blog del viajero/js/script.js
@@ -8,3 +8,21 @@ $(".fade-slider").jdSlider({
     isCursor: false,
     speed: 3000
 });
+
+var alturaBanner = $(".fade-slider").height();
+$(".bannerEstatico").css({"height": alturaBanner+"px"});
+
+/* ANIMACIONES SCROLL */
+$(window).scroll(() => {
+    var posY = window.pageYOffset;
+    if (posY >= alturaBanner) {
+        $("header").css({"background": "white"});
+        $("header .logotipo").css({"filter": "invert(100%)"});
+        $(".fa-search, .fa-bars").css({"color": "black"});
+        
+    } else {
+        $("header").css({"background": "rgba(0, 0, 0, 0.5)"});
+        $("header .logotipo").css({"filter": "invert(0%)"});
+        $(".fa-search, .fa-bars").css({"color": "white"});
+    }
+});
